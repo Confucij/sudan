9711e4771b92afd69c1e90c91e6b1aee1408ba03
Bohdan Shubenok <b.shubenok@flamesgroup.com>
2017-06-22 10:07:42 +0300

    Extend readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # sudan
-C call graph visualizer
+C call graph visualizer with stack sizes
+
+# Basic usage
+
+* code should be compiled with `-fdump-rtl-expand -fstack-usage`. This will generate `.expand` and `.su` file for each object file in project
+* all `.su` and `.expand` files should be combined in one file each respectively;
+* run `egupt` on `.expand` file:
+```
+egypt COMBINED.expand > COMBINED.egypt
+```
+* And finally run `sudan`:
+```
+python3 sudan.py --su COMBINED.su --exp COMBINED.egypt -d 30 -e stop_function_name -f function_to_analyze | dot -Tpdf -o output.pdf
+```
+arguments are:
+--su - combined su file
+--exp - combined egypt file
+-e - stop function. Analyzer would not go inside this function
--- sudan.py
+++ sudan.py
@@ -11,6 +11,8 @@ def render_default(fchains):
 
     conn = []
     for func, chains in fchains.items():
+        max_stack = 0
+        max_stack_chain = None
         local_conn = []
         for chain in chains:
             if not chain:
@@ -20,8 +22,22 @@ def render_default(fchains):
                 stack += chain[i+1][1]
                 local_conn.append('"{}:{}" -> "{}:{}" [label={}, style=solid];'.format(
                     chain[i][0], chain[i][1], chain[i+1][0], chain[i+1][1], stack))
+            if stack > max_stack:
+                max_stack = stack
+                max_stack_chain = chain
         local_conn = ['"{}:{}" [style=filled,fillcolor=green];\n'.format(func[0], func[1])] + list(set(local_conn))
         conn += local_conn
+        chain = max_stack_chain
+        max_chain = []
+        max_chain += ['{' + 'rank = same; "Maximum stack size: {} bytes" [color=pink, style=filled, fillcolor=pink];'.format(max_stack)]
+        stack = chain[0][1]
+        for i in range(0, len(chain) - 1):
+                stack += chain[i+1][1]
+                max_chain.append('"{}" -> "{}" [label={}, style=filled, fillcolor=red];'.format(
+                    chain[i][0], chain[i+1][0], stack))
+        conn.append(''.join(max_chain) + '}')
+        for node in max_stack_chain:
+            conn.append('"{}:{}" [style=filled, fillcolor=red];'.format(node[0], node[1]))
     return 'digraph callgraph {\nrankdir=LR;\n' + "".join(conn) + "\n}"
 
 
@@ -50,7 +66,7 @@ def parse_egypt_call_data(path):
             calls.append((match.group(1),match.group(2)))
     return calls
 
-def build_call_chains(ftuples, depth=10, finterest=None, fstacks=None, fexcept=None):
+def build_call_chains(ftuples, depth=100, finterest=None, fstacks=None, fexcept=None):
     """
 
     :type ftuples: list
@@ -120,11 +136,14 @@ def build_call_chains(ftuples, depth=10, finterest=None, fstacks=None, fexcept=N
 if __name__ == '__main__':
 
     options, args = getopt.getopt(sys.argv[1:], "d:f:e:", ["su=", "exp=", "except="])
+
     stack_sizes = None
     calls = []
     fexcept = []
     finterest = []
-    depth = 5
+    depth = 100
+    gen_pdf = False
+
 
     for opt, arg in options:
         if opt == "--su":
@@ -149,5 +168,6 @@ if __name__ == '__main__':
 
     chains = build_call_chains(calls, depth, finterest, stack_sizes, fexcept)
     print(render_default(chains))
+
 
     exit(0)
